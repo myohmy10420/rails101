faeb03db07548ffd0c5129d431eb909feca55977
KurtHsu <myohmy10420@gmail.com>
2016-08-24 15:30:37 +0800

    cant find ID

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-resource :welcome
+
     resource :groups
-    root'welcome#index'
+    root'groups#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
